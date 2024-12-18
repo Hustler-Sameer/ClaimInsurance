22acc9e229487bd8b4b513d220cc97c772d569f8
Hustler-Sameer <sameerkurkure10@gmail.com>
2024-12-18 18:15:36 +0530

    v1

--- src/components/header/header.component.ts
+++ src/components/header/header.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import {MatIconModule} from '@angular/material/icon';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatButtonModule} from '@angular/material/button';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [MatIconModule , MatDividerModule , MatButtonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
